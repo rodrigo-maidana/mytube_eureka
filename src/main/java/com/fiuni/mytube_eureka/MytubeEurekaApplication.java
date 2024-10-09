e5301bc2b6ebe607a8a7ff615a2165cf06d244a7
Rodrigo Maidana <114775631+romaidana29@users.noreply.github.com>
2024-10-09 19:52:41 -0300

    First Commit

---
+++ src/main/java/com/fiuni/mytube_eureka/MytubeEurekaApplication.java
@@ -0,0 +1,13 @@
+package com.fiuni.mytube_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytubeEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytubeEurekaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fiuni/mytube_eureka/MytubeEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiuni.mytube_eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MytubeEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
